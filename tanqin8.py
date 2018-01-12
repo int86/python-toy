c06441403756719055e9c9bf66cacdcace7c907b
Max <273206894@qq.com>
2018-01-12 17:39:20 +0800

    Update tanqin8.py

--- tanqin8.py
+++ tanqin8.py
@@ -2,18 +2,12 @@ from bs4 import BeautifulSoup
 import urllib
 import requests  
 import time
-def get_status(url):  
-    r = requests.get(url, allow_redirects = False)  
-    return r.status_code 
-
-def downImg(img,filename):
-	data = urllib.urlopen(img).read()
-	f = open(filename,'wb').write(data)
-
+#吉他谱地址
 url = "http://www.tan8.com/jitapu-55725.html"
 
 html = urllib.urlopen(url).read()
 
+#数据提取与清洗
 soup = BeautifulSoup(html,"html.parser")
 title = soup.h1.string
 img  =  soup.select('#img1')[0].img['src']
@@ -26,3 +20,14 @@ while(get_status(urlImg)==200):
 	downImg(urlImg,filename)
 	urlImg = urlImg.replace(str(count)+'.png',str(count+1)+'.png')
 	count = count + 1
+
+#获取网页状态码
+def get_status(url):  
+    r = requests.get(url, allow_redirects = False)  
+    return r.status_code 
+
+#下载图片
+def downImg(img,filename):
+	data = urllib.urlopen(img).read()
+	f = open(filename,'wb').write(data)
+
