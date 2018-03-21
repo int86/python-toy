6c8d6f18d5a0b78c320396d38d182d8905cf9d2a
Max <273206894@qq.com>
2018-03-21 18:03:58 +0800

    Update connectSSH.py

--- connectSSH.py
+++ connectSSH.py
@@ -1,49 +1,31 @@
 # encoding: UTF-8
-import pexpect
+from pexpect import pxssh
 
 """
 构建一个ssh网络
 依赖库：pexpect
 """
 
-PROMPT = ['#','>>>', '>','\$']
-def send_command(child, cmd):
-    child.sendline(cmd)
-    child.expect(PROMPT)
-    print(child.before)
+def send_cmd(s,cmd):
+	s.sendline(cmd)
+	s.prompt()
+	print s.before
 
-def connect(user, host, password,port):
-    ssh_newkey = 'Are you sure you want to continue connecting'
-    connStr = 'ssh '+'-p'+port+' '+user+'@'+host
-    child = pexpect.spawn(connStr)
-    try:
-        ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '%s@%s\'s [P|p]assword:' % (user,host)])
-    except:
-        print "[-] Error Connecting"
-        exit(0)
-    if ret == 0:
-        print '[-] Error Connecting'
-        return
-    if ret == 1:
-        child.sendline('yes')
-        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
-    if ret== 0:
-        print '[-] Error Connecting'
-        return
-    child.sendline(password)
-    child.expect(PROMPT)
-    if ret==0:
-        print '[-] Error Connecting, maybe password error'
-        exit(0)
-    return child
+def connect(host,user,passwd,p):
+	try:
+		s = pxssh.pxssh()
+		s.login(host,user,passwd,port=p)
+		return s
+	except:
+		print '[-] Error connecting'
+		exit(0)
 
-def main():
-    host = 'localhost'
-    user = 'root'
-    password=''
-    port = '22'
-    child = connect(user,host,password,port)
-    send_command(child,'ls -la')
 
 if __name__ == '__main__':
-    main()
+	host = 'localhost'
+	user = 'root'
+	passwd = ''
+	port = '22'
+	s = connect(host,user,passwd,port)
+	send_cmd(s,'ls -la')
+
